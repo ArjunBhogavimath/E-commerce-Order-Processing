83e9ccbf3d4c5b728bb16586766af08058656c9d
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2025-03-02 01:31:20 +0530

    Initial project setup

---
+++ src/main/java/com/ecommerce/E_commerce/Order/Processing/ECommerceOrderProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.E_commerce.Order.Processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceOrderProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceOrderProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/E_commerce/Order/Processing/ECommerceOrderProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.E_commerce.Order.Processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceOrderProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
